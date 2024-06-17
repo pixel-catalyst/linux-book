2cf2d9b7bdeebf39558ba3558be58863820d1bee
dark-matter-007 <ashwin.aksharma.p@outlook.com>
2024-06-17 18:26:34 +0530

    = fix gitbutler errors : improve human learning and some polishing

--- classes/database_controller.py
+++ classes/database_controller.py
@@ -1,13 +1,13 @@
 import sqlite3
+from typing import Optional, List, Any, Union
 
 
 class DatabaseController:
-    database_path = "../database.db"
+    database_path = "../resources/database.db"
     conn = None
     cursor = None
 
-    def ensure_existing(self, database_path="database.db"):
-        """Ensures that the database with the set path, by default database.db exists."""
+    def ensure_existing(self, database_path="./resources/database.db"):
         self.database_path = database_path
         conn = sqlite3.connect(self.database_path)
         conn.cursor().execute('''CREATE TABLE IF NOT EXISTS maintable
@@ -26,17 +26,19 @@ class DatabaseController:
         self.cursor.execute("INSERT INTO maintable VALUES (?, ?)", (key, value))
         self.conn.commit()
 
-    def get_value(self, key: str) -> str:
+    def get_value(self, key: str) -> Optional[list[Any]]:
         """
         Returns the value of a key
         :param key:
         :return value:
         """
-        self.cursor.execute("SELECT value FROM maintable WHERE key = ?", (key,))
+        self.cursor.execute("SELECT key, value FROM maintable WHERE key LIKE ?", (f"%{key}%",))
         try:
-            return self.cursor.fetchone()[0]
-        except Exception:
-            return None
+            return self.cursor.fetchall()
+        except Exception as e:
+            with open("dbLogs.txt", "w") as fil:
+                fil.write(str(e))
+            return "[ Not Found ]"
 
     def delete_pair(self, key: str) -> bool:
         try:
@@ -47,14 +49,22 @@ class DatabaseController:
             return False
 
     def update_value(self, key: str, value: str) -> None:
-        self.cursor.execute("UPDATE maintable SET value = ? WHERE key = ?", (value, key))
-        self.conn.commit()
+        try:
+            self.cursor.execute("UPDATE maintable SET value = ? WHERE key = ?", (value, key))
+            self.conn.commit()
+        except Exception as e:
+            print(e)
 
     def get_all_keys(self):
         self.cursor.execute("SELECT key FROM maintable")
         return [row[0] for row in self.cursor.fetchall()]
 
-    def get_key_by_value(self, value: str) -> str:
+    def get_key_by_value(self, value: str) -> Union[list[Any], str]:
         "Get the key of respective pair using the value as a key"
-        self.cursor.execute("SELECT key FROM maintable WHERE value = ?", (value,))
-        return self.cursor.fetchone()[0]
+        self.cursor.execute("SELECT key, value FROM maintable WHERE value LIKE ?", (f"%{value}%",))
+        try:
+            return self.cursor.fetchall()
+        except Exception as e:
+            with open("dbLogs.txt", "w") as fil:
+                fil.write(str(e))
+            return "[ Not Found ]"
